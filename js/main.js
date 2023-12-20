7367179f5975e467ca547d3a1acf0208bd7198c7
IgorTan14 <64653230+IgorTan14@users.noreply.github.com>
2023-12-20 11:35:19 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(function() {
+    $('.slider__items').slick({
+        arrows: false,
+        dots: true,
+        fade:true,
+        autoplay: true,
+        autoplaySpeed: 1000,
+    });
+    $('.bike-slider, .slider__item').slick({
+        arrows: false,
+        dots: true,
+        fade:true,
+        autoplay: true,
+        autoplaySpeed: 1000,
+    });
+});
